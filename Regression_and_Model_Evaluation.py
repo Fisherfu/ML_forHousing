65b0cc31b3ad53d6b80d5ccdea9bd822a9fa8989
Fisher <113712591+Fisherfu@users.noreply.github.com>
2024-03-11 06:00:49 +0800

    Update Regression_and_Model_Evaluation.py

--- Regression_and_Model_Evaluation.py
+++ Regression_and_Model_Evaluation.py
@@ -85,3 +85,47 @@ from sklearn.metrics import r2_score
 result=r2_score(y_test, y_pred)
 print(result)
 
+
+# Decision Tree Regression
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Training the Decision Tree Regression model on the Training set
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state = 0)
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+# Evaluating the Model Performance
+from sklearn.metrics import r2_score
+result=r2_score(y_test, y_pred)
+
+print(result)
+
+# Random Forest Regression
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Training the Random Forest Regression model on the whole dataset
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+# Evaluating the Model Performance
+from sklearn.metrics import r2_score
+result=r2_score(y_test, y_pred)
+
+print(result)
